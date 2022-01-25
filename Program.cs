c81ff07d41f05b6ffd977663344cf68ec25267e6
Taumren <thomas.lesur@viacesi.fr>
2022-01-25 15:57:19 +0100

    re-import from all

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Resources;
+using System.Reflection;
+using System.Threading;
+using System.Globalization;
+
+namespace EasySave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ResourceManager rm = new ResourceManager("EasySave.Resources.Strings", Assembly.GetExecutingAssembly());
+            
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("fr-FR");
+
+            Console.WriteLine(Thread.CurrentThread.CurrentUICulture.Name);
+            Console.WriteLine(rm.GetString("Language"));
+
+
+        }
+    }
+}
