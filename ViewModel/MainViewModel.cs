6163ee7fdc5a872c816440d23324b726726f2591
Taumren <thomas.lesur@viacesi.fr>
2022-01-26 10:13:17 +0100

    Comments added + update of language feature ?

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -6,17 +6,17 @@ namespace EasySave
 {
     class MainViewModel
     {
-
+        //Private attributes
         private string Language;
 
-
+        // publilc object from other classes 
         public View View;
 /*      public FileSaveManagement FileSaveManagement;
         public  LogManagement();*/
 
 
 
-
+        //Class Constructor 
         public MainViewModel()
         {
             View = new View();
@@ -24,19 +24,24 @@ namespace EasySave
                      LogManagement = new LogManagement();*/
             View.SetCLILanguage(GetUserLanguage());
             View.DisplayTranslatedMessage("Language");
+            System.Threading.Thread.Sleep(5000);
+            View.Clear();
+           
         }
 
+
+        //Method to get which Language the user wants to choose 
         public string GetUserLanguage()
         {
-            
+            // With this loop, users are forced to choose between FR or EN, if the choice is wrong it starts again
             while (true)
             { 
             string Message = "Choose your language / Choisissez votre langue : \n \n English: 1 \n Français: 2 \n \n Your choice / Votre choix :";
             View.DisplayBasicMessage(Message);
-
             string result = Console.ReadLine();
+            View.DisplayBasicMessage("\n ");
 
-            switch (result)
+                switch (result)
                 {
                     case "1":
                         return "en-US";
@@ -48,7 +53,6 @@ namespace EasySave
             }
 
         }
-
 
 
 
