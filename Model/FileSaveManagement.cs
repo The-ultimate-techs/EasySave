889c08ffe2f9be5241902077c90f7c0a71d4525c
HugoFern <hugo.fernandes@viacesi.fr>
2022-01-27 11:06:09 +0100

    Hugo_test

--- Model/FileSaveManagement.cs
+++ Model/FileSaveManagement.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace EasySave
 {
-    class FileSaveManagement
+    class FileSaveManagement:FileSave
     {
+        public void CreateSaveFile(string Title= null, string SourceDirectory= null, string DestinationDirectory= null, string Type= null)
+        {
+            System.IO.FileStream file = File.Create(DestinationDirectory);
+            System.IO.File.SetAttributes
+
+        }
+
     }
 }
