39da03fe3ba04cc01cd85d308801d3fc4336fe78
Taumren <thomas.lesur@viacesi.fr>
2022-01-28 21:28:44 +0100

    Add Create / Edit / Delete SafeFile Feature

--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -13,7 +14,7 @@ namespace EasySave
 
         // publilc object from other classes 
         public View View;
-        //public FileSaveManagement FileSaveManagement;
+        public FileSaveManagement FileSaveManagement;
         public LogManagement LogManagement;
 
 
@@ -23,7 +24,7 @@ namespace EasySave
         public MainViewModel()
         {
             View = new View();
-            //FileSaveManagement = new FileSaveManagement();
+            FileSaveManagement = new FileSaveManagement();
             LogManagement = new LogManagement();
             View.SetCLILanguage(GetUserLanguage());
             View.DisplayTranslatedMessage("Language");
@@ -41,6 +42,7 @@ namespace EasySave
 
             do
             {
+                View.Clear();
                 View.DisplayTranslatedMessage("MainMenu");
                
                 string result = Console.ReadLine();
@@ -50,17 +52,17 @@ namespace EasySave
                 {
                     case "1":
                         SetMenuChoice(result);
+                        SubMenuCreate();
                         break;
 
                     case "2":
                         SetMenuChoice(result);
+                        SubMenuEditDelete();
                         break;
 
                     case "3":
                         SetMenuChoice(result);
-                        LogManagement.BeginEndProcess();
-                        LogManagement.RunningLogGénérator("Test", "Source Dir","C thomas",500,848484,45); 
-                        LogManagement.BeginEndProcess();
+                       
                         break;
 
                     case "4":
@@ -86,6 +88,198 @@ namespace EasySave
         }
 
 
+        public void SubMenuCreate()
+        {
+            View.Clear();
+            View.DisplayTranslatedMessage("SubMenuCreate");
+            DisplayListSaveFile();
+
+            View.DisplayTranslatedMessage("SubMenuCreate2");
+            View.DisplayTranslatedMessage("SubMenuCreate.Title");
+            string Title = Console.ReadLine();
+            View.DisplayTranslatedMessage("SubMenuCreate.SourcePath");
+            string SourcePath = Console.ReadLine();
+            View.DisplayTranslatedMessage("SubMenuCreate.DestPath");
+            string DestPath = Console.ReadLine();
+
+            string Type;
+            do
+            {
+                View.DisplayTranslatedMessage("SubMenuCreate.Type");
+
+                Type = Console.ReadLine();
+            } while (Type != "1" && Type != "2");
+
+            
+            SaveFileJson SaveFileJson = new SaveFileJson();
+            SaveFileJson.Title = Title;
+            SaveFileJson.SourcePath = SourcePath;
+            SaveFileJson.DestPath = DestPath;
+            SaveFileJson.Type = (Type == "1") ? "COMPLETE" : "PARTIAL"; ;
+
+            string JsonPath = FileSaveManagement.GetSaveFileDirectory() + Title + ".Json";
+
+
+            string json = JsonConvert.SerializeObject(SaveFileJson, Formatting.Indented);
+
+            using (StreamWriter sw = File.CreateText(JsonPath))
+            {
+
+                sw.WriteLine(json);
+
+            }
+
+        }
+
+
+        public void SubMenuEditDelete()
+        {
+
+
+
+            do
+            {
+                View.Clear();
+                DisplayListSaveFile();
+                View.DisplayTranslatedMessage("SubMenuEditDelete");
+                View.DisplayTranslatedMessage("SubMenuEditDelete.choice");
+                string result = Console.ReadLine();
+                View.DisplayBasicMessage("\n ");
+
+                switch (result)
+                {
+                    case "1":
+
+                        SetMenuChoice(result);
+                        string FileToEdit;
+                        do
+                        {
+                            
+                            View.Clear();
+                            DisplayListSaveFile();
+                            View.DisplayTranslatedMessage("SubMenuEditDelete.choice2");
+                            FileToEdit = Console.ReadLine();
+
+
+                            bool isNumeric = int.TryParse(FileToEdit, out _);
+                            if (isNumeric ==  false )
+                            {
+                                FileToEdit = "0";
+                            }
+
+                        } while (Convert.ToInt32(FileToEdit) > ListSaveFile().Count || Convert.ToInt32(FileToEdit) <= 0);
+
+
+                        View.Clear();
+
+                       
+                        View.DisplayTranslatedMessage("SubMenuCreate.SourcePath");
+                        string SourcePath = Console.ReadLine();
+                        View.DisplayTranslatedMessage("SubMenuCreate.DestPath");
+                        string DestPath = Console.ReadLine();
+
+                        string Type;
+                        do
+                        {
+                            View.DisplayTranslatedMessage("SubMenuCreate.Type");
+
+                            Type = Console.ReadLine();
+                        } while (Type != "1" && Type != "2");
+
+
+                        FileSave FileSave = new FileSave();
+                        FileSave = ListSaveFile()[Convert.ToInt32(FileToEdit) - 1];
+
+                        SaveFileJson EditFileJson = new SaveFileJson();
+                        EditFileJson.Title = FileSave.GetTitle().Remove(5);
+                        EditFileJson.SourcePath = SourcePath;
+                        EditFileJson.DestPath = DestPath;
+                        EditFileJson.Type = (Type == "1") ? "COMPLETE" : "PARTIAL"; ;
+
+
+                        string JsonPath = FileSaveManagement.GetSaveFileDirectory() + EditFileJson.Title ;
+
+
+                        string json = JsonConvert.SerializeObject(EditFileJson, Formatting.Indented);
+
+
+                        string AreyousureEdit;
+                        do
+                        {
+
+                            View.Clear();
+                            View.DisplayTranslatedMessage("Areyousure");
+                            AreyousureEdit = Console.ReadLine();
+
+
+                        } while (AreyousureEdit != "1" && AreyousureEdit != "2");
+
+                        if (AreyousureEdit == "1")
+                        {
+                            using (StreamWriter sw = File.CreateText(JsonPath))
+                            {
+
+                                sw.WriteLine(json);
+
+                            }
+                        }
+
+
+                        break;
+
+                    case "2":
+
+                        string FileToDelete;
+                        SetMenuChoice(result);
+                        do
+                        {
+                            
+                            View.Clear();
+                            DisplayListSaveFile();
+                            View.DisplayTranslatedMessage("SubMenuEditDelete.choice2");
+                            FileToDelete = Console.ReadLine();
+
+
+                            bool isNumeric = int.TryParse(FileToDelete, out _);
+                            if (isNumeric == false)
+                            {
+                                FileToDelete = "0";
+                            }
+
+                        } while (Convert.ToInt32(FileToDelete) > ListSaveFile().Count || Convert.ToInt32(FileToDelete) <= 0);
+
+                        string AreyousureDelete;
+                        do
+                        {
+                            
+                            View.Clear();
+                            View.DisplayTranslatedMessage("Areyousure");
+                            AreyousureDelete = Console.ReadLine();
+
+
+                        } while (AreyousureDelete != "1" && AreyousureDelete != "2");
+
+                        if (AreyousureDelete == "1")
+                        {
+                            FileSave FileToDeleteInfo = new FileSave();
+                            FileToDeleteInfo = ListSaveFile()[Convert.ToInt32(FileToDelete)-1];
+                            File.Delete(FileSaveManagement.GetSaveFileDirectory() + FileToDeleteInfo.GetTitle());
+                        }
+
+                       
+                        break;
+
+                    case "3":
+                        SetMenuChoice(result);
+                        break;
+
+
+
+                }
+
+            } while (GetMenuChoice() != "3");
+
+        }
 
 
         //Method to get which Language the user wants to choose 
@@ -108,6 +302,7 @@ namespace EasySave
                     case "2":
                         SetMenuChoice("fr-FR");
                         break;
+
                 }
 
             } while (GetMenuChoice() != "en-US" && GetMenuChoice() != "fr-FR");
@@ -115,6 +310,25 @@ namespace EasySave
         }
 
 
+        public void DisplayListSaveFile()
+        {
+            View.DisplayTranslatedMessage("ListSaveFile");
+            List<FileSave> ListFile = new List<FileSave> { };
+            ListFile = FileSaveManagement.GetFilesOnADirectory(FileSaveManagement.GetSaveFileDirectory(), "");
+
+            int index = 1;
+            foreach (FileSave FileSave in ListFile)
+            {
+                View.DisplayBasicMessage("\n n°" + index + " -->  " + FileSave.GetTitle());
+                index++;
+            }
+        }
+
+        public List<FileSave> ListSaveFile()
+        {
+            return FileSaveManagement.GetFilesOnADirectory(FileSaveManagement.GetSaveFileDirectory(), "");
+
+        }
 
 
         // Setter & getter for Language
@@ -139,6 +353,8 @@ namespace EasySave
             return this.MenuChoice;
         }
 
+
+        
 
 
 
