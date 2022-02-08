0423a88fbd59856f8d6d144edb8dcd5b6dab5fcf
HugoFern <hugo.fernandes@viacesi.fr>
2022-02-08 09:11:17 +0100

    Add MemoryManager Class

---
+++ Model/MemoryManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+//namespace EasySave.Model
+namespace EasySave
+{
+    public class MemoryManager
+    {
+        public long MaximumAllocatedMemory; // In Bytes
+
+        private void StartGarbageCollector()
+        {
+            GC.Collect();
+            Console.WriteLine("The garbage had been collected, MaximumAllocatedMemory : {0} , GetTotalMemory : {1}", MaximumAllocatedMemory, GC.GetTotalMemory(false));
+        }
+
+        public void SetMaximumAllocatedMemory(int bytes)
+        {
+            MaximumAllocatedMemory = bytes;
+            //Console.WriteLine("The MaximumAllocatedMemory had been setted, MaximumAllocatedMemory : {0} , GetTotalMemory : {1}", MaximumAllocatedMemory, GC.GetTotalMemory(false));
+        }
+
+        public void TryStartGarbageCollector()
+        {
+
+            while (Thread.CurrentThread.IsAlive)
+            {
+                //Console.WriteLine("MaximumAllocatedMemory : {0} , GetTotalMemory : {1}", MaximumAllocatedMemory, GC.GetTotalMemory(false));
+                if (MaximumAllocatedMemory < GC.GetTotalMemory(false))
+                {
+                    StartGarbageCollector();
+                }
+            }
+            
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,8 +11,42 @@ namespace EasySave
     {
         static void Main(string[] args)
         {
+            #region MainViewModel
+            //MainViewModel MainViewModel = new MainViewModel();
+            #endregion
 
-            MainViewModel MainViewModel = new MainViewModel();
+            #region Thread Garbage Collector
+            MemoryManager MemoryManager = new MemoryManager();
+            MemoryManager.SetMaximumAllocatedMemory(75000);
+            //Console.WriteLine("AAAAAAA MaximumAllocatedMemory : {0} , GetTotalMemory : {1}", MemoryManager.MaximumAllocatedMemory, GC.GetTotalMemory(false));
+
+
+            //Thread Garbage Collector
+
+            Thread InstanceCaller = new Thread(new ThreadStart(MemoryManager.TryStartGarbageCollector));
+
+
+            //Instance caller
+            InstanceCaller.Start();
+            #endregion
+
+            Version vt;
+
+            // Create objects and release them to fill up memory with unused objects.
+            while(true)
+            {
+                //Console.WriteLine("An object will be created, MaximumAllocatedMemory : {0} , GetTotalMemory : {1}", MemoryManager.MaximumAllocatedMemory, GC.GetTotalMemory(false));
+                vt = new Version();
+                Thread.Sleep(2000);
+                //Console.WriteLine("An object has been created, MaximumAllocatedMemory : {0} , GetTotalMemory : {1}", MemoryManager.MaximumAllocatedMemory, GC.GetTotalMemory(false));
+            }
+
+            /*Console.WriteLine("Memory used before collection:       {0:N0}",GC.GetTotalMemory(false));
+
+            
+            // Collect all generations of memory.
+            GC.Collect();
+            Console.WriteLine("Memory used after full collection:   {0:N0}",GC.GetTotalMemory(true));*/
 
 
         }
